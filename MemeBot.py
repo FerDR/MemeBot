1a7001e28099901340fe6b9a88e4dc17f4981777
FerDR <fer.delrio.445@gmail.com>
2021-04-09 20:54:27 +0200

    Added multitemplate support

--- MemeBot.py
+++ MemeBot.py
@@ -15,23 +15,27 @@ logger = logging.getLogger(__name__)
 
 templates = {
         "badness":{
-            "box" : (175,437,332,465),
+            "box" : [(175,437,332,465)],
             "color"  : (166,49,58)
             },
         "classnote":{
-            "box" : (403,475,545,563),
+            "box" : [(403,475,545,563)],
+            "color" : (0,0,0)
+            },
+        "drake":{
+            "box" : [(368,38,652,296),(368,368,652,652)],
             "color" : (0,0,0)
             },
         "nofear":{
-            "box" : (285,108,400,233),
+            "box" : [(285,108,400,233)],
             "color" : (0,0,0)
             },
         "spider":{
-            "box" : (75,470,220,538),
+            "box" : [(75,470,220,538)],
             "color" : (0,0,0)
             },
         "worthless":{
-            "box" : (98,73,333,203),
+            "box" : [(98,73,333,203)],
             "color" : (0,0,0)
             },
 
@@ -39,7 +43,8 @@ templates = {
 
 def help(update,context):
     message = """Use \list to see all available templates,
-    then make a meme by doing \<template name> <text>"""
+    then make a meme by doing \<template name> <text>.
+    Separate multi-texts with an underscore"""
     update.message.reply_text(message)
 
 def list_templates(update,context):
@@ -50,20 +55,30 @@ def list_templates(update,context):
     update.message.reply_text(message)
 
 def make_meme(update,context,template):
-    text = ''
-    for arg in context.args:
-        text+=arg
-        text+=" "
-    if not text: text = "YOUR TEXT HERE "
-    text = text[0:-1]
+    Nboxes = len(templates[template]["box"])
+    rawtext = context.args
+    fittext = [l.split(',') for l in ','.join(rawtext).split('_')]
+    if len(fittext)!=Nboxes:
+        update.message.reply_text("Please make sure that the number of texts matches the number of places")
+        return 
+    text = []
+    for ip, phrase in enumerate(fittext):
+        text.append("")
+        for word in phrase:
+            text[ip]+=word
+            text[ip]+=" "
+        if not text[ip]: text = "YOUR TEXT HERE "
+        text[ip] = text[ip][0:-1]
     color = templates[template]["color"]
-    x1,y1,x2,y2 = templates[template]["box"]
-    Dx = x2-x1
-    Dy = y2-y1
     img = Image.open('Templates/{}.png'.format(template))
     draw = ImageDraw.Draw(img)
-    wrap,size = BU.get_wrapped_text(text,draw,Dx,Dy)
-    draw.text((x1,y1,x2,y2),wrap,font=BU.get_font(size),fill=color)
+    boxes = templates[template]["box"]
+    for ib, box in enumerate(boxes):
+        x1,y1,x2,y2 = box
+        Dx = x2-x1
+        Dy = y2-y1
+        wrap,size = BU.get_wrapped_text(text[ib],draw,Dx,Dy)
+        draw.text((x1,y1,x2,y2),wrap,font=BU.get_font(size),fill=color)
     img.save("meme.png")
     png = open("meme.png",'rb')
     update.message.reply_photo(png)
