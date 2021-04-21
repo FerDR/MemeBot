06ab2422572508e65648c016dc873fb67e79f4ce
FerDR <fer.delrio.445@gmail.com>
2021-04-21 20:31:29 +0200

    Bug-fixing

--- MemeBot.py
+++ MemeBot.py
@@ -41,7 +41,7 @@ templates = {
 
         }
 
-def help(update,context):
+def Help(update,context):
     message = """Use \list to see all available templates,
     then make a meme by doing \<template name> <text>.
     Separate multi-texts with an underscore"""
@@ -57,6 +57,10 @@ def list_templates(update,context):
 def make_meme(update,context,template):
     Nboxes = len(templates[template]["box"])
     rawtext = context.args
+    if not rawtext:
+        rawtext = []
+        for n in range(Nboxes):
+            rawtext.append("YOUR TEXT HERE")
     fittext = [l.split(',') for l in ','.join(rawtext).split('_')]
     if len(fittext)!=Nboxes:
         update.message.reply_text("Please make sure that the number of texts matches the number of places")
@@ -67,7 +71,6 @@ def make_meme(update,context,template):
         for word in phrase:
             text[ip]+=word
             text[ip]+=" "
-        if not text[ip]: text = "YOUR TEXT HERE "
         text[ip] = text[ip][0:-1]
     color = templates[template]["color"]
     img = Image.open('Templates/{}.png'.format(template))
@@ -87,6 +90,7 @@ def main():
     token = BU.getAccessToken('token.txt')
     bot = Updater(token,use_context=True)
     dispatcher = bot.dispatcher
+    dispatcher.add_handler(CommandHandler("help",Help))
     dispatcher.add_handler(CommandHandler("list",list_templates))
     for key in templates:
         dispatcher.add_handler(CommandHandler(key,functools.partial(make_meme,template=key)))
